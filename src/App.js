e170e3a88a22dbb0313fbfd41361f87b27fc6724
Jeff Benak <jeff.benak@yahoo.com>
2022-07-05 17:29:47 -0500

    Fixed a minor error in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Container from "./components/container";
 
 function App() {
   return (
-    <div className="App">]
+    <div className="App">
     <Container></Container>
 
     </div>
