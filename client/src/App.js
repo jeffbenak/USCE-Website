9da5944dbfdc232cc0bc89243e09079709d0d28f
Jeff Benak <jeff.benak@yahoo.com>
2022-07-05 17:25:08 -0500

    Moved public to the root directory and changed develop name to client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from "./components/container";
+
+function App() {
+  return (
+    <div className="App">]
+    <Container></Container>
+
+    </div>
+  );
+}
+
+export default App;
