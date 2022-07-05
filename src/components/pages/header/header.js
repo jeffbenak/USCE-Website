c337f69e292a273e93ed00aeab1b3b3b9438c8c3
Jeff Benak <jeff.benak@yahoo.com>
2022-07-05 17:27:24 -0500

    Moved src to the root directory

---
+++ src/components/pages/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Header() {
+    return (
+        <div>
+        <h2><FontAwesomeIcon></FontAwesomeIcon></h2>
+        </div>
+    );
+}
