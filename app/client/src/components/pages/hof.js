531c7a63c3f0ed830db75477d1cabb4748d0df73
Jeff Benak <jeff.benak@yahoo.com>
2022-09-03 21:27:28 -0500

    Added filler content to the hall of fame page just like the U.S rotations page, and added a google fonts as well and some css to the image and text

--- app/client/src/components/pages/hof.js
+++ app/client/src/components/pages/hof.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Header from '../header/header'
+import Footer from '../footer/footer'
+import Student from '../../images/doctor2.jpg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 
@@ -7,8 +12,25 @@ export default function Hof() {
     return (
       <div>
         <Header></Header>
-        <h1>Hall of Fame</h1>
-        <p>
+        <Container>
+          <Row className='fame'>
+            <Col>
+
+        <div>
+        <img className='studimg' src={Student}></img>
+       </div>
+            </Col>
+
+            <Col>
+
+            <div className='doctitle'>
+              <h4>Dr Jashank Parwani</h4>
+            <br></br>
+            <h5 className='matched'>MATCHED to Neurology in Temple University from Lokmanya Tilak Municipal Medical College, Mumbai, India</h5>
+            </div>
+            <br></br>
+          <h2 className='review'>Review</h2>
+          <p className='studrev'>
           Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
           Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
           mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
@@ -16,6 +38,19 @@ export default function Hof() {
           imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
           in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
         </p>
+
+            </Col>
+
+          </Row>
+
+        
+
+        </Container>
+
+        <br></br>
+
+        <Footer></Footer>
+
       </div>
     );
   }
