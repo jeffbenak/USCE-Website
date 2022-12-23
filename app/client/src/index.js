c0c57b476cbdf213fd87e3245f0f158065d37e0d
Jeff Benak <jeff.benak@yahoo.com>
2022-12-22 20:47:52 -0600

    Deleted chatroom because it's not being used and deleted services temporarily

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router, Route, Routes, useRoutes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 <App />
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
